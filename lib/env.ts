c745a79178a7073bf9515211265582c31509fe53
Abi Dzar Nashrullah Al <abidzar@Abidzars-Macbook-Pro.local>
2026-04-13 11:47:33 +0700

    fix: add ARCJET_KEY to env config

--- lib/env.ts
+++ lib/env.ts
@@ -5,6 +5,7 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().min(1),
     LMS_JWT_SECRET: z.string().min(32),
+    ARCJET_KEY: z.string().optional()
   },
   experimental__runtimeEnv: {},
 });
